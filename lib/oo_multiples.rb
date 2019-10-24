43144fa87cda0c9249496ea8130511f8ed9014ae
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2019-10-24 11:51:27 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+    attr_reader :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        arr = (1...@limit).to_a
+        multiples = arr.select {|i| i % 3 == 0 || i % 5 == 0}
+    end
+    
+    def sum_multiples
+        multiples = collect_multiples
+        multiples.sum
+    end
+
+end
