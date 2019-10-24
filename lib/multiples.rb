40804559dbf168e082d15ecf0855cc9e9b550aa9
NaveedG <nganjani@protonmail.com>
2019-10-24 09:39:59 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
-# Enter your procedural solution here!
+#procedural solution
+def collect_multiples(num)
+  range = (1...num).to_a
+  range.select { |x| (x % 3 == 0) || (x % 5 == 0) }
+end
+
+def sum_multiples(num)
+  collect_multiples(num).reduce(:+)
+end
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
-# Enter your object-oriented solution here!
+#object-oriented solution
+class Multiples
+  attr_reader :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  def collect_multiples
+    range = (1...@num).to_a
+    range.select { |x| (x % 3 == 0) || (x % 5 == 0) }
+  end
+
+  def sum_multiples
+    collect_multiples.reduce(:+)
+  end
+
+end
